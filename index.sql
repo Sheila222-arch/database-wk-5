45cd2cbcf3bd0953e37677929e6b9aeba6e11875
SHEILA <sheilahodima@gmail.com>
2025-09-08 11:24:36 +0300

    Add files via upload

---
+++ index.sql
@@ -0,0 +1,8 @@
+    QUESTION 1
+    DROP INDEX  idxPhone ON customer;
+    QUESTION 2
+    CREATE USER 'bob''@'localhost' IDENTIFIED BY 'S!cu3R3!';
+    QUESTION 3
+    GRANT INSERT ON salesDB.* to'bob'@'localcost';
+    QUESTION 4
+    ALTER USER 'bob'@'localcost' IDENTIFIED BY 'P&55!23';
